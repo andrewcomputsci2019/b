d691205d0e1efd06870ec5df7ca2b8ba06ec4160
Andrew pegg <andrewcomputsci2019@gmail.com>
2019-10-23 14:27:30 -0700

    oofy woofy

---
+++ second project/second product.c
@@ -0,0 +1,15 @@
+//
+//  second product.c
+//  second project
+//
+//  Created by Pegg, Andrew on 10/23/19.
+//  Copyright © 2019 Pegg, Andrew. All rights reserved.
+//
+
+#include "second product.h"
+int project2()
+{
+    printf("biggy oof, see what i did there micheal");
+   return 0;
+}
+
---
+++ second project/second product.h
@@ -0,0 +1,15 @@
+//
+//  second product.h
+//  second project
+//
+//  Created by Pegg, Andrew on 10/23/19.
+//  Copyright © 2019 Pegg, Andrew. All rights reserved.
+//
+
+#ifndef second_product_h
+#define second_product_h
+
+#include <stdio.h>
+int project2(void);
+
+#endif /* second_product_h */

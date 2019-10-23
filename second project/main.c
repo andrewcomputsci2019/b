bae8414e2a0a46f77b880f0681f4f887217eb5e4
Andrew pegg <andrewcomputsci2019@gmail.com>
2019-10-23 14:23:43 -0700

    Initial Commit

---
+++ second project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  second project
+//
+//  Created by Pegg, Andrew on 10/23/19.
+//  Copyright © 2019 Pegg, Andrew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
